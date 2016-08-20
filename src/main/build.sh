b8d7e684a8c8b90c7de8df9ab1ca0c73371cf387
Jemy <jemygraw@gmail.com>
2016-08-20 21:56:51 +0800

    remove old code

--- src/main/build.sh
+++
@@ -1,3 +0,0 @@
-export GOPATH=/home/jemy/Coding/qdisksync:$GOPATH
-go build qdisksync
-go build qdisksync.go
--- src/main/fsize.py
+++
@@ -1,7 +0,0 @@
-fp=open("qdisksync.cache")
-fsize_all=0
-for line in fp:
-	items=line.split("\t")
-	fsize_all+=int(items[1])
-fp.close()
-print("Size:"+str(fsize_all/1024.0/1024.0)+"MB")
